2adecff537ed9fc516dd307bbffa331ae8a3bddc
Cora Marcu <cora.marcu@gmail.com>
2022-02-27 13:58:16 +0000

    finish the reachDestination kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,12 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  if (typeof distance !== "number" || typeof speed !== "number") {
+    return "Please pass numbers as arguments.";
+  }
+
+  const time = distance / speed;
+  const roundedTime = Math.round(time * 2) / 2;
+
+  return `I should be there in ${roundedTime} hours.`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,18 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  const obj = { name: "objectArg" };
+  const arr = [1, "two"];
+  test("returns error message if passed argument is not a number", () => {
+    expect(reachDestination(obj, 12)).toEqual(
+      "Please pass numbers as arguments."
+    );
+    expect(reachDestination(34, arr)).toEqual(
+      "Please pass numbers as arguments."
+    );
+  });
+
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+  });
 });
