7fda29cb85939e7cc8bd18ca76c516c6468a9ffa
Cora Marcu <cora.marcu@gmail.com>
2022-02-27 13:08:08 +0000

    finish humanCatDogYears kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,27 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = (number) => {
+  let catAge = 0;
+  let dogAge = 0;
+
+  if (number <= 0) {
+    return "Enter a valid age of 1 or above.";
+  }
+
+  if (number === 1) {
+    catAge = 15;
+    dogAge = 15;
+  }
+
+  if (number === 2) {
+    catAge = 24;
+    dogAge = 24;
+  }
+
+  if (number >= 3) {
+    catAge = 24 + (number - 2) * 4;
+    dogAge = 24 + (number - 2) * 5;
+  }
+
+  return [number, catAge, dogAge];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,32 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns an array", () => {
+    expect(humanCatDogYears(1)).toEqual(expect.any(Array));
+  });
+
+  it("returns error message if number passed is 0 or smaller", () => {
+    expect(humanCatDogYears(0)).toEqual("Enter a valid age of 1 or above.");
+    expect(humanCatDogYears(-3)).toEqual("Enter a valid age of 1 or above.");
+  });
+
+  it("returns an array of length 3", () => {
+    expect(humanCatDogYears(1)).toHaveLength(3);
+    expect(humanCatDogYears(20)).toHaveLength(3);
+    expect(humanCatDogYears(93)).toHaveLength(3);
+  });
+
+  it("returns the correct ages for number passed: 1", () => {
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+  });
+
+  it("returns the correct ages for number passed: 2", () => {
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+  });
+
+  it("returns the correct ages for number passed: >= 3", () => {
+    expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+    expect(humanCatDogYears(12)).toEqual([12, 64, 74]);
+    expect(humanCatDogYears(18)).toEqual([18, 88, 104]);
+  });
+});
