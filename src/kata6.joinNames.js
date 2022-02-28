557545f5115138eaf41d65b7bbd1eed536440e24
Cora Marcu <cora.marcu@gmail.com>
2022-02-28 18:07:40 +0000

    complete joinNames kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,25 @@
-const joinNames = namesObj => {};
+const joinNames = (namesObj) => {
+  // checks that the argument is an array
+  if (!Array.isArray(namesObj)) {
+    return "Please pass an array of Objects.";
+  }
+  // checks that all elements in array are objects with a name property
+  for (object of namesObj) {
+    if (typeof object !== "object") {
+      return "Please pass an array of Objects.";
+    }
+    if (!object.hasOwnProperty("name")) {
+      return "Please pass an array of Objects.";
+    }
+  }
+
+  const namesArr = namesObj.map((obj) => obj.name);
+  const lastName = namesArr.splice(namesArr.length - 1, 1);
+  const namesStr = namesArr.reduce((acc, curr) => {
+    return acc + `, ${curr}`;
+  });
+
+  return `${namesStr} & ${lastName}`;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,26 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  const numArr = [1, 2, 3, 4];
+  const randomArr = [{ name: "someName" }, 1, "a thing", true];
+  test("returns error message if not called with an array of objects", () => {
+    expect(joinNames(numArr)).toEqual("Please pass an array of Objects.");
+    expect(joinNames(randomArr)).toEqual("Please pass an array of Objects.");
+    expect(joinNames("Tom, Jerry")).toEqual("Please pass an array of Objects.");
+  });
+
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Alex" }, { name: "Jess" }, { name: "Jack" }])
+    ).toEqual("Alex, Jess & Jack");
+
+    expect(
+      joinNames([
+        { name: "Maya" },
+        { name: "Flynn" },
+        { name: "Dory" },
+        { name: "Rascal" },
+      ])
+    ).toEqual("Maya, Flynn, Dory & Rascal");
+  });
 });
